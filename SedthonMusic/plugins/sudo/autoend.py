f56b3be27270c9f5b1e6fbb50737c3d1fd34e7da
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-09-30 03:28:02 +0300

    Update autoend.py

--- SedthonMusic/plugins/sudo/autoend.py
+++ SedthonMusic/plugins/sudo/autoend.py
@@ -11,10 +11,10 @@ from pyrogram import filters
 
 import config
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import autoend_off, autoend_on
-from YukkiMusic.utils.decorators.language import language
+from SedthonMusic import app
+from SedthonMusic.misc import SUDOERS
+from SedthonMusic.utils.database import autoend_off, autoend_on
+from SedthonMusic.utils.decorators.language import language
 
 # Commands
 AUTOEND_COMMAND = get_command("AUTOEND_COMMAND")
