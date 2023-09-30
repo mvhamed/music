bb162bddafd5aaa12f7a3a0f08719b0b7e68bb52
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-09-30 03:19:10 +0300

    Update ping.py

--- SedthonMusic/plugins/tools/ping.py
+++ SedthonMusic/plugins/tools/ping.py
@@ -15,10 +15,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS, MUSIC_BOT_NAME, PING_IMG_URL
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.core.call import Yukki
-from YukkiMusic.utils import bot_sys_stats
-from YukkiMusic.utils.decorators.language import language
+from SedthonMusic import app
+from SedthonMusic.core.call import Yukki
+from SedthonMusic.utils import bot_sys_stats
+from SedthonMusic.utils.decorators.language import language
 
 ### Commands
 PING_COMMAND = get_command("PING_COMMAND")
