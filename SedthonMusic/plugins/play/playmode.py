24fb08333213a298f9fa007e3a21012bd6886e44
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-09-30 03:39:11 +0300

    Update playmode.py

--- SedthonMusic/plugins/play/playmode.py
+++ SedthonMusic/plugins/play/playmode.py
@@ -13,11 +13,11 @@ from pyrogram.types import InlineKeyboardMarkup, Message
 
 from config import BANNED_USERS
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.utils.database import (get_playmode, get_playtype,
+from SedthonMusic import app
+from SedthonMusic.utils.database import (get_playmode, get_playtype,
                                        is_nonadmin_chat)
-from YukkiMusic.utils.decorators import language
-from YukkiMusic.utils.inline.settings import playmode_users_markup
+from SedthonMusic.utils.decorators import language
+from SedthonMusic.utils.inline.settings import playmode_users_markup
 
 ### Commands
 PLAYMODE_COMMAND = get_command("PLAYMODE_COMMAND")
