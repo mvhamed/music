68192aa64b78d1005cefd0f555b0fadfb00de39c
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-09-30 03:17:31 +0300

    Update songs.py

--- SedthonMusic/plugins/tools/songs.py
+++ SedthonMusic/plugins/tools/songs.py
@@ -22,10 +22,10 @@ from pyrogram.types import (InlineKeyboardButton,
 from config import (BANNED_USERS, SONG_DOWNLOAD_DURATION,
                     SONG_DOWNLOAD_DURATION_LIMIT)
 from strings import get_command
-from YukkiMusic import YouTube, app
-from YukkiMusic.utils.decorators.language import language, languageCB
-from YukkiMusic.utils.formatters import convert_bytes
-from YukkiMusic.utils.inline.song import song_markup
+from SedthonMusic import YouTube, app
+from SedthonMusic.utils.decorators.language import language, languageCB
+from SedthonMusic.utils.formatters import convert_bytes
+from SedthonMusic.utils.inline.song import song_markup
 
 # Command
 SONG_COMMAND = get_command("SONG_COMMAND")
