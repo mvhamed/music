aba4966d79a89eb2f988868d22354fa2fb541d2a
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-09-30 03:12:20 +0300

    Update play.py

--- SedthonMusic/utils/decorators/play.py
+++ SedthonMusic/utils/decorators/play.py
@@ -12,15 +12,15 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from config import PLAYLIST_IMG_URL, PRIVATE_BOT_MODE, adminlist
 from strings import get_string
-from YukkiMusic import YouTube, app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import (get_cmode, get_lang,
+from SedthonMusic import YouTube, app
+from SedthonMusic.misc import SUDOERS
+from SedthonMusic.utils.database import (get_cmode, get_lang,
                                        get_playmode, get_playtype,
                                        is_active_chat,
                                        is_commanddelete_on,
                                        is_served_private_chat)
-from YukkiMusic.utils.database.memorydatabase import is_maintenance
-from YukkiMusic.utils.inline.playlist import botplaylist_markup
+from SedthonMusic.utils.database.memorydatabase import is_maintenance
+from SedthonMusic.utils.inline.playlist import botplaylist_markup
 
 
 def PlayWrapper(command):
