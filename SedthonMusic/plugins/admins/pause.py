f321ae8e6911b788fcf97e91bc7093916403d065
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-09-30 03:35:42 +0300

    Update pause.py

--- SedthonMusic/plugins/admins/pause.py
+++ SedthonMusic/plugins/admins/pause.py
@@ -13,10 +13,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.core.call import Yukki
-from YukkiMusic.utils.database import is_music_playing, music_off
-from YukkiMusic.utils.decorators import AdminRightsCheck
+from SedthonMusic import app
+from SedthonMusic.core.call import Yukki
+from SedthonMusic.utils.database import is_music_playing, music_off
+from SedthonMusic.utils.decorators import AdminRightsCheck
 
 # Commands
 PAUSE_COMMAND = get_command("PAUSE_COMMAND")
