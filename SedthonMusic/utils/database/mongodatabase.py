692538f645b8a79ee708237b8a09d141be269818
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-09-30 03:14:11 +0300

    Update mongodatabase.py

--- SedthonMusic/utils/database/mongodatabase.py
+++ SedthonMusic/utils/database/mongodatabase.py
@@ -10,7 +10,7 @@
 
 from typing import Dict, List, Union
 
-from YukkiMusic.core.mongo import mongodb
+from SedthonMusic.core.mongo import mongodb
 
 queriesdb = mongodb.queries
 userdb = mongodb.userstats
