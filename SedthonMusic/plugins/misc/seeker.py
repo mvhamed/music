b815b6ee2691689a71dd518d3e7273613769c620
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-09-30 03:29:50 +0300

    Update seeker.py

--- SedthonMusic/plugins/misc/seeker.py
+++ SedthonMusic/plugins/misc/seeker.py
@@ -13,11 +13,11 @@ import asyncio
 from pyrogram.types import InlineKeyboardMarkup
 
 from strings import get_string
-from YukkiMusic.misc import db
-from YukkiMusic.utils.database import (get_active_chats, get_lang,
+from SedthonMusic.misc import db
+from SedthonMusic.utils.database import (get_active_chats, get_lang,
                                        is_music_playing)
-from YukkiMusic.utils.formatters import seconds_to_min
-from YukkiMusic.utils.inline import (stream_markup_timer,
+from SedthonMusic.utils.formatters import seconds_to_min
+from SedthonMusic.utils.inline import (stream_markup_timer,
                                      telegram_markup_timer)
 
 from ..admins.callback import wrong
