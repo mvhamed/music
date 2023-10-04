abfab61cf1a468463c7d101ea91ba4e355263c8a
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-10-04 17:20:43 +0300

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -40,12 +40,12 @@ for filename in os.listdir(r"./strings"):
 for filename in os.listdir(r"./strings/langs/"):
     if "en" not in languages:
         languages["en"] = yaml.safe_load(
-            open(r"./strings/langs/ar.yml", encoding="utf8")
+            open(r"./strings/langs/en.yml", encoding="utf8")
         )
         languages_present["en"] = languages["en"]["name"]
     if filename.endswith(".yml"):
         language_name = filename[:-4]
-        if language_name == "ar":
+        if language_name == "en":
             continue
         languages[language_name] = yaml.safe_load(
             open(r"./strings/langs/" + filename, encoding="utf8")
