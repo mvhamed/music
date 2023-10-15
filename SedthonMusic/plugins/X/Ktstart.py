62c04a33e8ea98f7546f345461935127eba59c17
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-10-15 21:20:30 +0300

    Update Ktstart.py

--- SedthonMusic/plugins/X/Ktstart.py
+++ SedthonMusic/plugins/X/Ktstart.py
@@ -6,9 +6,9 @@ from sys import version_info
 from time import time
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from YukkiMusic import app
-from YukkiMusic import settingsApp
-from YukkiMusic.utils.decorators.admins import AdminActual
+from SedthonMusic import app
+from SedthonMusic import settingsApp
+from SedthonMusic.utils.decorators.admins import AdminActual
 from strings import get_command
 from strings.filters import command
 
