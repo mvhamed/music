ce1828a8e1fc61e960d286184ba188ee2116dc79
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-11-09 18:50:25 +0300

    Update كشف.py

--- SedthonMusic/plugins/X/كشف.py
+++ SedthonMusic/plugins/X/كشف.py
@@ -16,7 +16,7 @@ from asyncio import gather
 
 
 
-@app.on_message(command(["معلوماته", "كشف"]) & filters.group & ~filters.edited) 
+@app.on_message(command(["معلوماته", "كشف"]) & filters.group) 
 async def hshs(client: Client, message: Message):      
     usr = await client.get_users(message.reply_to_message.from_user.id)
     name = usr.first_name#
