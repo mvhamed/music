aafafd88075c6cbae4c68baf90d9b1368bfd2f75
mvhamed <145144709+mvhamed@users.noreply.github.com>
2023-12-07 19:45:12 +0200

    Update thumbnails.py

--- SedthonMusic/utils/thumbnails.py
+++ SedthonMusic/utils/thumbnails.py
@@ -30,52 +30,72 @@ def changeImageSize(maxWidth, maxHeight, image):
     return newImage
 
 
-async def gen_thumb(client, username, photo):
-        if os.path.isfile(f"{username}.png"):
-           return f"{username}.png"
-        users = len(await get_served_users(client))
-        chats = len(await get_served_chats(client))
-        url = f"https://www.youtube.com/watch?v=gKA2XFkJZhI"
+async def gen_thumb(videoid, photo):
+    if os.path.isfile(f"{photo}.png"):
+        return f"{photo}.png"
+
+    url = f"https://www.youtube.com/watch?v={videoid}"
+    try:
         results = VideosSearch(url, limit=1)
         for result in (await results.next())["result"]:
+            try:
+                title = result["title"]
+                title = re.sub("\W+", " ", title)
+                title = title.title()
+                test = translator.translate(title, dest="en")
+                title = test.text
+            except:
+                title = "Unsupported Title"
+            try:
+                duration = result["duration"]
+            except:
+                duration = "Unknown Mins"
             thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+            try:
+                views = result["viewCount"]["short"]
+            except:
+                views = "Unknown Views"
+            try:
+                channel = result["channel"]["name"]
+            except:
+                channel = "Unknown Channel"
 
         async with aiohttp.ClientSession() as session:
             async with session.get(thumbnail) as resp:
                 if resp.status == 200:
-                    f = await aiofiles.open(f"thumb{username}.png", mode="wb")
+                    f = await aiofiles.open(
+                        f"thumb{videoid}.png", mode="wb"
+                    )
                     await f.write(await resp.read())
                     await f.close()
 
-        youtube = Image.open(f"cache/thumb{videoid}.png")
+        youtube = Image.open(f"thumb{videoid}.png")
+        SEMOv = Image.open(f"{photo}")
         image1 = changeImageSize(1280, 720, youtube)
         image2 = image1.convert("RGBA")
-        background = image2.filter(filter=ImageFilter.BoxBlur(30))
+        background = image2.filter(filter=ImageFilter.BoxBlur(5))
         enhancer = ImageEnhance.Brightness(background)
         background = enhancer.enhance(0.6)
-        Xcenter = youtube.width / 2
-        Ycenter = youtube.height / 2
+        Xcenter = SEMOv.width / 2
+        Ycenter = SEMOv.height / 2
         x1 = Xcenter - 250
         y1 = Ycenter - 250
         x2 = Xcenter + 250
         y2 = Ycenter + 250
-        logo = youtube.crop((x1, y1, x2, y2))
+        logo = SEMOv.crop((x1, y1, x2, y2))
         logo.thumbnail((520, 520), Image.ANTIALIAS)
         logo = ImageOps.expand(logo, border=15, fill="white")
         background.paste(logo, (50, 100))
         draw = ImageDraw.Draw(background)
-        font = ImageFont.truetype("assets/font2.ttf", 40)
-        font2 = ImageFont.truetype("assets/font2.ttf", 70)
-        arial = ImageFont.truetype("assets/font2.ttf", 30)
-        name_font = ImageFont.truetype("assets/font.ttf", 30)
+        font = ImageFont.truetype("font2.ttf", 40)
+        font2 = ImageFont.truetype("font2.ttf", 70)
+        arial = ImageFont.truetype("font2.ttf", 30)
+        name_font = ImageFont.truetype("font.ttf", 30)
         para = textwrap.wrap(title, width=32)
         j = 0
         draw.text(
-            (5, 5), f"{MUSIC_BOT_NAME}", fill="white", font=name_font
-        )
-        draw.text(
             (600, 150),
-            "NOW PLAYING",
+            "NoNa PlAYiNg",
             fill="white",
             stroke_width=2,
             stroke_fill="white",
@@ -122,10 +142,11 @@ async def gen_thumb(client, username, photo):
             font=arial,
         )
         try:
-            os.remove(f"cache/thumb{videoid}.png")
+            os.remove(f"{photo}")
+            os.remove(f"thumb{videoid}.png")
         except:
             pass
-        background.save(f"cache/{videoid}.png")
-        return f"cache/{videoid}.png"
+        background.save(f"{photo}.png")
+        return f"{photo}.png"
     except Exception:
-        return YOUTUBE_IMG_URL
+        return ahmed
