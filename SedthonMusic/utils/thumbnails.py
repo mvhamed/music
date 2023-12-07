94468602c1e65f94234e145e0a31ce78e5ab01f0
mvhamed <145144709+mvhamed@users.noreply.github.com>
2023-12-07 19:29:01 +0200

    Update thumbnails.py

--- SedthonMusic/utils/thumbnails.py
+++ SedthonMusic/utils/thumbnails.py
@@ -30,40 +30,20 @@ def changeImageSize(maxWidth, maxHeight, image):
     return newImage
 
 
-async def gen_thumb(videoid):
-    if os.path.isfile(f"cache/{videoid}.png"):
-        return f"cache/{videoid}.png"
-
-    url = f"https://www.youtube.com/watch?v={videoid}"
-    try:
+async def gen_bot(client, username, photo):
+        if os.path.isfile(f"{username}.png"):
+           return f"{username}.png"
+        users = len(await get_served_users(client))
+        chats = len(await get_served_chats(client))
+        url = f"https://www.youtube.com/watch?v=gKA2XFkJZhI"
         results = VideosSearch(url, limit=1)
         for result in (await results.next())["result"]:
-            try:
-                title = result["title"]
-                title = re.sub("\W+", " ", title)
-                title = title.title()
-            except:
-                title = "Unsupported Title"
-            try:
-                duration = result["duration"]
-            except:
-                duration = "Unknown Mins"
             thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-            try:
-                views = result["viewCount"]["short"]
-            except:
-                views = "Unknown Views"
-            try:
-                channel = result["channel"]["name"]
-            except:
-                channel = "Unknown Channel"
 
         async with aiohttp.ClientSession() as session:
             async with session.get(thumbnail) as resp:
                 if resp.status == 200:
-                    f = await aiofiles.open(
-                        f"cache/thumb{videoid}.png", mode="wb"
-                    )
+                    f = await aiofiles.open(f"thumb{username}.png", mode="wb")
                     await f.write(await resp.read())
                     await f.close()
 
